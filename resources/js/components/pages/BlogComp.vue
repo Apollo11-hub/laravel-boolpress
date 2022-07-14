f378caf7eca88ef7f16f922c3f2d3e9dbbcff1a2
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-07-14 20:04:40 +0200

    Inserimento post in pagina blog

--- resources/js/components/pages/BlogComp.vue
+++ resources/js/components/pages/BlogComp.vue
@@ -1,10 +1,46 @@
 <template>
-    <h1>blogvue</h1>
+    <div>
+        <h1>Tutti i Post</h1>
+        <ul>
+
+            <li v-for="post in posts" :key="post.id">
+
+
+                <h1>{{post.title}}</h1>
+                <p>{{post.contenet}}</p>
+            </li>
+
+        </ul>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
 name:'BlogComp',
+
+    data() {
+        return {
+            posts:null
+        }
+    },
+    mounted() {
+
+        this.getApi()
+
+
+    },
+
+    methods: {
+        getApi(){
+            axios.get('/api/posts')
+            .then(res =>{
+
+                this.posts=res.data;
+            })
+        }
+    },
 }
 </script>
 
